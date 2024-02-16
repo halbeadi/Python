13ef41eb6144516a1d357d9a43b100972ecc11a9
Aditya <halbeadi@gmail.com>
2024-02-16 13:38:44 +0530

    new example code

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,11 @@
-print("Hello World")
+i = 0
+even_sum = 0
+odd_sum = 0
+while(i <= 10):
+  if(i%2==0):
+    even_sum = even_sum + i
+  else:
+    odd_sum = odd_sum + i
+    i = i + 1
+print(even_sum, odd_sum)
+  
